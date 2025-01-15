73d9360291410c01a131d267bb0f3b150652aab3
Ian Chin Wang <ian.chin.wang@oracle.com>
2025-01-15 10:26:30 -0500

    Add basic structure with net/http
    Signed-off-by: Ian Chin Wang <ian.chin.wang@oracle.com>

---
+++ api/server.go
@@ -0,0 +1,71 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"go.uber.org/zap"
+)
+
+// Defines missing consts in the API Spec
+const (
+	ApplicationvndVeraisonCharesJson string = "application/vnd.veraison.chares+json"
+)
+
+type Server struct {
+	logger *zap.SugaredLogger
+}
+
+func NewServer(logger *zap.SugaredLogger) *Server {
+	return &Server{
+		logger: logger,
+	}
+}
+
+func (s *Server) returnBadRequest(w http.ResponseWriter, r *http.Request, errMsg string) {
+	s.logger.Error(errMsg)
+	badRequestError := &BadRequestError{
+		Detail: &errMsg,
+		Status: N400,
+		Title:  InvalidRequest,
+		Type:   TagGithubCom2024VeraisonratsdErrorInvalidrequest,
+	}
+	w.WriteHeader(http.StatusBadRequest)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(badRequestError)
+}
+
+func (s *Server) RatsdChares(w http.ResponseWriter, r *http.Request, param RatsdCharesParams) {
+	var requestData ChaResRequest
+
+	// Check if content type matches the expectation
+	ct := r.Header.Get("Content-Type")
+	if ct != ApplicationvndVeraisonCharesJson {
+		errMsg := fmt.Sprintf("wrong content type, expect %s (got %s)", ApplicationvndVeraisonCharesJson, ct)
+		s.returnBadRequest(w, r, errMsg)
+		return
+	}
+
+	respCt := fmt.Sprintf("application/eat+jwt; eat_profile=%s", TagGithubCom2024Veraisonratsd)
+	if *(param.Accept) != respCt {
+		errMsg := fmt.Sprintf("wrong accept type, expect %s (got %s)", respCt, *(param.Accept))
+		s.returnBadRequest(w, r, errMsg)
+		return
+	}
+
+	payload, _ := io.ReadAll(r.Body)
+	err := json.Unmarshal(payload, &requestData)
+	if err != nil {
+		errMsg := "fail to retrieve nonce from request"
+		s.returnBadRequest(w, r, errMsg)
+		return
+	}
+
+	s.logger.Info("request nonce: ", requestData.Nonce)
+	s.logger.Info("request media type: ", *(param.Accept))
+	w.Header().Set("Content-Type", respCt)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("hello, rastd!"))
+}
