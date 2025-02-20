35a9798471fdf02a3a9198b99c23a7b3ef7e4798
Ian Chin Wang <ian.chin.wang@oracle.com>
2025-02-20 16:33:53 -0500

    Add configfs-TSM subattester
    Add the configfs-TSM which generates CMWs for TSM report. If TSM is
    unaviable from the OS, no supported format will be returned.
    
    Signed-off-by: Ian Chin Wang <ian.chin.wang@oracle.com>

---
+++ attesters/tsm/tsm_test.go
@@ -0,0 +1,57 @@
+// Copyright 2025 Contributors to the Veraison project.
+// SPDX-License-Identifier: Apache-2.0
+package tsm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/google/go-configfs-tsm/configfs/linuxtsm"
+	"github.com/stretchr/testify/assert"
+	"github.com/veraison/ratsd/proto/compositor"
+)
+
+func Test_getEvidenceError(t *testing.T) {
+	e := fmt.Errorf("sample error")
+
+	expected := &compositor.EvidenceOut {
+		Status: &compositor.Status{
+			Result: false, Error: "sample error",
+		},
+	}
+
+	assert.Equal(t, expected, getEvidenceError(e))
+}
+
+func Test_GetSubAttesterID(t *testing.T) {
+	p := &TSMPlugin{}
+
+	expected := &compositor.SubAttesterIDOut {
+		SubAttesterID: sid,
+		Status:        statusSucceeded,
+	}
+
+	assert.Equal(t, expected, p.GetSubAttesterID())
+}
+
+
+func Test_GetSupportedFormats(t *testing.T) {
+	p := &TSMPlugin{}
+	var expected *compositor.SupportedFormatsOut
+
+	if _, err := linuxtsm.MakeClient(); err != nil {
+		expected = &compositor.SupportedFormatsOut {
+			Status:  &compositor.Status{
+				Result: false,
+				Error: fmt.Sprintf("TSM is not available: %s", err.Error()),
+			},
+		}
+	} else {
+		expected = &compositor.SupportedFormatsOut{
+			Status:  statusSucceeded,
+			Formats: supportedFormats,
+		}
+	}
+
+	assert.Equal(t, expected, p.GetSupportedFormats())
+}
