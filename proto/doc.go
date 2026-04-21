7002daf7250df3a7d8750c3aca43552330a5d80b
Thomas Fossati <thomas.fossati@linaro.org>
2026-04-21 18:17:05 +0200

    fix: reinstate file that was mistakenly deleted
    Signed-off-by: Thomas Fossati <thomas.fossati@linaro.org>

---
+++ proto/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2025 Contributors to the Veraison project.
+// SPDX-License-Identifier: Apache-2.0
+
+package proto
+
+//go:generate protoc --go_out=. --go_opt=module=github.com/veraison/ratsd/proto --go-grpc_out=. --go-grpc_opt=module=github.com/veraison/ratsd/proto compositor.proto
